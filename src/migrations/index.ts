6a9d5ff5574844c16f37c4d4da7f3eb4c3265bbc
Rob Boskind <49008703+mysticfalconvt@users.noreply.github.com>
2025-12-24 16:43:47 -0500

    add a migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251221_112306 from './20251221_112306';
 import * as migration_20251221_183343 from './20251221_183343';
+import * as migration_20251224_214242 from './20251224_214242';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20251221_183343.up,
     down: migration_20251221_183343.down,
-    name: '20251221_183343'
+    name: '20251221_183343',
+  },
+  {
+    up: migration_20251224_214242.up,
+    down: migration_20251224_214242.down,
+    name: '20251224_214242'
   },
 ];
